05bf29b9b344e136d9c220bfff5bde3d29a305b1
Nathan Neilson <nbn1343@gmail.com>
2025-02-21 11:59:02 -0700

    Adding add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println ("Testing Dockerfile");
+    Calculator calculator = new Calculator();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println ("welcome to the calculator");
+    System.out.println ("Enter a command:");
+
+    while (scanner.hasNextLine()) {
+      String input = scanner.nextLine();
+      String[] parts = input.split(" ");
+      String command = parts[0].toLowerCase();
+
+      switch (command) {
+        case "add":
+          int a1 = Integer.parseInt(parts[1]);
+          int b1 = Integer.parseInt(parts[2]);
+          System.out.println(calculator.add(a1, b1));
+          break;
+      }
+    }
   }
 }
